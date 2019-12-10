7de456396609373d520e245df64b9e28eb7ed85c
E650009 <E650009@HMC-YJ-N040385.hkmc.hkmg.global>
2019-12-10 19:33:52 +0900

    Add Hi world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!");
 		System.out.println("A yo");
+		System.out.println("Hi World!");
 	}
 }
